f672ec79515b54eb3bd2fa7c74f39bc9101d563c
wbbinchina <1031901644@qq.com>
2020-12-26 23:55:09 +0800

    Second

--- app/src/main/java/com/example/wangbinbin/wangbinbin2018110341.kt
+++ app/src/main/java/com/example/wangbinbin/wangbinbin2018110341.kt
@@ -3,6 +3,7 @@ package com.example.wangbinbin
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.AbsSpinner
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -15,29 +16,15 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        fun result(spinner: AbsSpinner, firstNumber:Int, secondNumber:Int): Int {
-            val result:Int = when(spinner.selectedItem.toString()){
-                "+" -> firstNumber + secondNumber
-                "-" -> firstNumber - secondNumber
-                "*" -> firstNumber * secondNumber
-                "/" -> firstNumber / secondNumber
-                else -> -1
-            }
-            return result
-            }
 
-        button.setOnClickListener {
-            val second = second_edit.text.toString().toInt()
-            val first = first_edit.text.toString().toInt()
-            textView.text = result(spinner,first,second).toString()
-            val data=first_edit.text.toString()+spinner.selectedItem.toString()+second_edit.text.toString()+textView.text
-            datas.add(data)
-        }
-        button3.setOnClickListener {
+        button_record.setOnClickListener {
             val intent=Intent(this,Second_Activity::class.java)
             intent.putStringArrayListExtra(First,datas)
             startActivity(intent)
         }
+    }
+
+    fun onNumber(view: View) {
 
     }
 }
